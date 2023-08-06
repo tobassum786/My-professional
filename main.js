32446407c52b82c205d676cf38ecdf9993773fff
tobassum786 <tobassum@msn.com>
2023-08-06 19:40:45 +0300

    modify model

--- main.js
+++ main.js
@@ -15,22 +15,22 @@ const toggleMenu = () => {
 }
 
 menuToggleIcon.addEventListener('click', toggleMenu);
+
 ////Model popup
-//document.addEventListener('click', (e)=> {
-//    if(e.target.classList.contains('service__btn')) {
-//        toggleModelPopup();
-//        popupDetails(e.target.parentElement);
-//    }
-//})
-//
-//function toggleModelPopup() {
-//    document.querySelector('.model__wrapper').classList.toggle('open');
-//}
-//
-//document.querySelector('.close__btn').addEventListener('click', toggleModelPopup);
-//
-//function popupDetails(modelItems) {
-//    document.querySelector(".model__thumbnail img").src = modelItems.querySelector(".service__img").src;
-//    document.querySelector(".model__details").innerHTML = modelItems.querySelector(".service__detail").innerHTML;
-//}
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('open__model')) {
+        toggleModelPopup();
+        popupDetails(e.target.parentElement);
+    }
+})
+
+function toggleModelPopup() {
+    selectElement('.model').classList.toggle('open');
+}
+
+selectElement('.close_btn').addEventListener('click', toggleModelPopup);
+
+function popupDetails(modelItems) {
+    selectElement(".model__content").innerHTML = modelItems.querySelector(".service__card-detail").innerHTML;
+}
 ///Intersection animation

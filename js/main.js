58f516b6b6cb54548bdfd0cd965b94cbe9398bf2
Tobassum Munir <tobassum@msn.com>
2025-06-06 19:07:02 +0300

    fix rendring issues

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//Grap items
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if (element) return element;
+    throw new Error(`Something went wrong, please check ${element} typed correctly`)
+};
+
+//Responsive toggle menu
+const menuToggleIcon = selectElement('#toggle__btn');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+////Model popup
+
+//FAQs sections
+const faqBtn = document.querySelectorAll('.faq__menu');
+
+faqBtn.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('show')
+        toggle.classList.toggle('show')
+    });
+});
+///Intersection animation

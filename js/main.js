ae0495580a26cb2aa0947bf2bee95ef709610448
Tobassum Munir <tobassum@msn.com>
2025-06-08 08:30:33 +0300

    fix header

--- js/main.js
+++ js/main.js
@@ -6,15 +6,26 @@ const selectElement = selector => {
 };
 
 //Responsive toggle menu
-const menuToggleIcon = selectElement('#toggle__btn');
+const menuToggleIcon = selectElement('#toggle-btn');
 
 const toggleMenu = () => {
     const mobileMenu = selectElement('#menu');
-    mobileMenu.classList.toggle('activated');
-    menuToggleIcon.classList.toggle('activated');
+    mobileMenu.classList.toggle('active');
+    menuToggleIcon.classList.toggle('active');
 }
 
 menuToggleIcon.addEventListener('click', toggleMenu);
+
+// Close menu when menu links are clicked
+const menuLinks = document.querySelectorAll('.nav-links a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const mobileMenu = selectElement('#menu');
+        const menuToggleIcon = selectElement('#toggle-btn');
+        mobileMenu.classList.remove('active');
+        menuToggleIcon.classList.remove('active');
+    });
+});
 
 ////Model popup
 
